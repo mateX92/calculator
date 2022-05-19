ee35726ce81bf504f3752008ce733040a7cbfe05
Mateusz <matkub1992@gmail.com>
2022-05-19 12:02:53 +0200

    Add decimals, percentage, reverse the number and clear
    Decimals: add one dot to insert a number with decimals
    Percentage: divide the number by 100 to get a percentage
    Reverse: change the number to negative or back to positive
    Clear: remove the value inserted previously (as opposed to clear all)

--- functions.js
+++ functions.js
@@ -1,6 +1,5 @@
 
 // Main math functions
-
 let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
@@ -20,23 +19,38 @@ function operate(operator, a, b) {
 
 // Display
 const display = document.querySelector("h1"); // display
+
+// Buttons
 const numButtons = document.querySelectorAll(".num"); // 0 - 9
 const operateButtons = document.querySelectorAll(".operator"); // +, -, *, /, =
 const equals = document.querySelector('#equals') // =
 const clearBtn = document.querySelector('#clear'); // AC
+const percent = document.querySelector('#percent'); // %
+const reverse = document.querySelector('#reverse'); // reverse number
+const coma = document.querySelector('#coma');
+const backBtn = document.querySelector('#back');
 
+// Initial Values
 let displayValue = []; // add clicked numbers to array
 let displayNumbers;  // joined array
 let operations = ['a','operator','b']; 
 let click = 0;
 let result = 0;
+let comaInput = 0; // no decimals
+let numberReversed = 0; // number is positive
+let percentage = 0; // percentage disabled
 
+// Numbers
 numButtons.forEach((button) => {
     button.addEventListener('click', function() {
         insertNumbers(button);
     });
 });
 
+// Decimals
+coma.addEventListener('click', inputDecimals);
+
+// Operators
 operateButtons.forEach((button) => {
     button.addEventListener('click', function() {
         insertOperand(button);
@@ -47,6 +61,7 @@ equals.addEventListener('click', function() {
     insertEqual();
 });
 
+// CLEAR ALL DATA
 clearBtn.addEventListener('click', function clear() {
     result = 0;
     displayValue = [];
@@ -54,13 +69,61 @@ clearBtn.addEventListener('click', function clear() {
     displayNumbers = 0;
     display.textContent = 0;
     click = 0;
+    comaInput = 0;
+    numberReversed = 0;
+    percentage = 0;
+});
+
+// Clear last only
+backBtn.addEventListener('click', function() {
+    displayValue.pop();
+    displayNumbers = displayValue.join("");
+    display.textContent = displayNumbers;
+})
+
+// CHANGE POSITIVE NUMBER TO A NEGATIVE ONE
+reverse.addEventListener('click', function reverseNumber() { 
+    if(numberReversed === 0) {
+        displayValue.unshift('-');
+        numberReversed = 1;
+    } else if(numberReversed === 1) {
+        displayValue.shift('-');
+        reverse.removeEventListener('click',reverseNumber,true);
+        numberReversed = 0;
+    }
+    displayNumbers = displayValue.join("");
+    display.textContent = displayNumbers;
+});
+
+// PERCENT BUTTON
+percent.addEventListener('click', function changeToPercent() {
+    if(percentage === 0) {
+        displayNumbers = Number(displayNumbers) / 100;
+        percentage = 1;
+    } else if(percentage === 1) {
+        displayNumbers = Number(displayNumbers) * 100;
+        percentage = 0;
+    };
+    display.textContent = displayNumbers;
 });
 
 function insertNumbers(button) {
-    displayValue.push(button.innerText); 
+    displayValue.push(button.innerText);
     displayNumbers = displayValue.join("");
     display.textContent = displayNumbers;
+    
 };
+
+function inputDecimals() {
+    if(comaInput === 0) {
+        displayValue.push(coma.innerText); 
+        displayNumbers = displayValue.join("");
+        display.textContent = displayNumbers;
+        comaInput++;
+    } else {
+        coma.removeEventListener('click',inputDecimals,true);
+    }
+}
 
 function insertOperand(button) {
     if(click < 1) {
